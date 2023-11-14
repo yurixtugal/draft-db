96d7cb4c98b435ec0b5af4c512f370151b2ca96d
yurixtugal <yurixtugal12@gmail.com>
2023-11-14 00:22:16 -0500

    Change styles

--- components/ui/showScript.tsx
+++ components/ui/showScript.tsx
@@ -20,16 +20,16 @@ const ShowScript = ({ script }: { script: string }) => {
 
 
   return (
-    <div className="bg-[#292a2e] rounded-md pl-6 h-full">
-      <h2 className="text-xl pl-4 text-white mb-2 bg-[#292a2e] flex">
+    <div className=" rounded-md pl-6 h-full">
+      <h2 className="text-xl pl-4 text-white mb-2  flex">
         DDL Script{" "}
         {copied ? <CopyCheck className="pl-3 h-7 w-7 text-neutral-200" /> : <Copy className="pl-3 h-7 w-7 hover:cursor-pointer text-neutral-400 hover:text-neutral-200" 
         onClick={onCopy}
         />}
         {copied && <span className="text-base text-neutral-200 pl-3 ">Copied!</span>}
       </h2>
-      <ScrollArea className="rounded-md p-4 mr-6 h-4/6 w-5/5">
-        <pre className="text-sm text-green-300 bg-gray-900 rounded-md p-4">
+      <ScrollArea className="rounded-md p-4 mr-6 h-4/6 w-5/5 border-2 border-green-500">
+        <pre className="text-sm text-green-300  rounded-md p-4">
         <ReactTypingEffect text={script} speed={2} eraseDelay={10} />
         </pre>
         <ScrollBar orientation="horizontal" />
