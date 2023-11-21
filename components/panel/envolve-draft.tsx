0e2e911992ca0ec0d8e22fd6621027a083eddc01
yurixtugal <yurixtugal12@gmail.com>
2023-11-20 20:55:38 -0500

    Add delete route

--- components/panel/envolve-draft.tsx
+++ components/panel/envolve-draft.tsx
@@ -7,6 +7,8 @@ import { parseToMermaid } from "@/lib/mermaidUtils/util";
 import dynamic from "next/dynamic";
 import { generateDDL } from "@/lib/ddl-generator/ddl-generator";
 import { ScrollArea } from "../ui/scroll-area";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
 interface DrawDraftInterface {
   draft: DraftWithCollection;
@@ -17,10 +19,28 @@ const ComponentC = dynamic(() => import("@/components/panel/mermaid-flow"), {
 });
 
 const DrawDraft = ({ draft }: DrawDraftInterface) => {
+  
+  const router = useRouter();
+
+  const deleteDraft = async () => {
+    if (confirm("Are you sure you want to delete this draft?")){
+      console.log(draft.idDraft)
+      const res = await fetch(`/api/draft/${draft.idDraft}`, {
+        method: "DELETE",
+      });
+      
+      router.push("/");
+
+    }
+  }
+  
   return (
     <ScrollArea  className="h-[calc(100vh-10rem)]" >
       <div className="pl-10 mr-8">
-        <div className="pb-2 pr-5 text-2xl text-white mb-3">{draft?.name}</div>
+        <div className="flex flex-row justify-between pb-2 pr-5 text-2xl text-white mb-3">
+          <span>{draft?.name}</span>
+          
+        </div>
         <div className="pb-2 mr-5 text-sm text-gray-200 mb-6">{draft?.description}</div>
         <div className="">
           <ComponentC draft={draft} />
